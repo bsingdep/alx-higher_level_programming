776253f57938fa5af3d286f51091806e3c807753
bsingdep <singdepbenjamin@gmail.com>
2022-12-04 16:49:21 +0000

    python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
+
 def multiple_returns(sentence):
-    my_tuple = ()
-    if len(sentence) == 0:
-        my_tuple = 0, "None"
+    if sentence == "":
+        first_chr = None
     else:
-        my_tuple = len(sentence), sentence[0]
-    return
+        first_chr = sentence[0]
+    return len(sentence), first_chr
