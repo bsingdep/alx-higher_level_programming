7591fcae50703183e394735b65cd07e0f00b1257
bsingdep <singdepbenjamin@gmail.com>
2023-01-10 15:35:29 +0000

    updated

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,57 +1,31 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+
+"""
+File: 10-student.py
+Desc: This module deals with classes and json
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Aug 2 2022
+"""
 
 
-class Student:
-    """Represent a student."""
-
+class Student():
+    """
+    A simple stundent class.
+    """
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+        """
+        This function initializes the object parameters.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__#!/usr/bin/python3
-"""Defines a class Student."""
-
-
-class Student:
-    """Represent a student."""
-
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+        This function retrieves a dictionary representation
+        of a Student instance
         """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return ({key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)})
         return self.__dict__
