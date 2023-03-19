f4ae3d70eca0d1a9c55965d3c4e5e62cb15b3b00
bsingdep <singdepbenjamin@gmail.com>
2023-03-19 13:46:41 +0000

    askelon

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-
-"""
-File: 5-filter_cities.py
-Desc: This module contains a python script that takes in the
-name of a state as an argument and lists all cities of that
-state, using the database hbtn_0e_4_usa
-Author: Gizachew Bayness (Elec Crazy)
-Date Created: Oct 7 2022
-"""
-
-from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name,
-            states.name FROM cities
-            INNER JOIN states
-            ON cities.state_id = states.id
-            ORDER BY cities.id ASC""")
-    states_info = cur.fetchall()
-
-    filtered_list_state = ([state[1] for state in states_info
-                            if state[2] == argv[4]])
-    print(", ".join(filtered_list_state))
-
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
